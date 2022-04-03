846752961efaeabe9772e4b20513f9a4820852e2
root <root@localhost>
2022-04-03 09:51:41 +0000

    MakinglocalBakup

--- src/contracts/NFTMarket.sol
+++ src/contracts/NFTMarket.sol
@@ -3,8 +3,71 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Market {
+contract Ownable is Context {
+    address private _owner;
+    address private _buybackOwner;
+
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    modifier onlyBuybackOwner() {
+        require(
+            _buybackOwner == _msgSender(),
+            "Ownable: caller is not the buyback owner"
+        );
+        _;
+    }
+
+    constructor() {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        _buybackOwner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    function transferOwnership(address newOwner) external virtual onlyOwner {
+        require(
+            newOwner != address(0),
+            "Ownable: new owner is the zero address"
+        );
+
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+
+    function transferBuybackOwnership(address newOwner)
+        external
+        virtual
+        onlyOwner
+    {
+        require(
+            newOwner != address(0),
+            "Ownable: new owner is the zero address"
+        );
+
+        emit OwnershipTransferred(_buybackOwner, newOwner);
+        _buybackOwner = newOwner;
+    }
+
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    function buybackOwner() public view returns (address) {
+        return _buybackOwner;
+    }
+}
+
+contract Market is Ownable {
 	enum ListingStatus {
 		Active,
 		Sold,
@@ -17,6 +80,7 @@ contract Market {
 		address token;
 		uint tokenId;
 		uint price;
+		uint256 tokenAmount;
 	}
 
 	event Listed(
@@ -25,6 +89,7 @@ contract Market {
 		address token,
 		uint tokenId,
 		uint price,
+		uint256 tokenAmount,
         uint quantity
 	);
 
@@ -34,6 +99,7 @@ contract Market {
 		address token,
 		uint tokenId,
 		uint price,
+		uint256 tokenAmount,
         uint quantity
 	);
 
@@ -44,20 +110,34 @@ contract Market {
 
 	uint private _listingId = 0;
 	mapping(uint => Listing) private _listings;
-	address Owner = 0xDa4E9AE390720DE2b73d2520bBda344aEaCbEFD8;
-	uint Percentage = 1;
+	address nftOwner = 0xDa4E9AE390720DE2b73d2520bBda344aEaCbEFD8;
+	address nullAddr = 0x0000000000000000000000000000000000000000;
+	uint Percentage = 1;          
+	address private nativeToken = 0xBa007b6170C602C08545ff97395677408688D3a2;
 
- 
-	function setOwner(address _owner) external{
-		Owner = _owner;
+	function setNativeToken(address _token) external onlyOwner{
+		require(_token != 0x0000000000000000000000000000000000000000, "Native token cannot be null address");
+		nativeToken = _token;
 	}
 
-	function setPercentage(uint _percentage) external{
+	function setNFTOwner(address _owner) external onlyOwner{
+		nftOwner = _owner;
+	}
+
+	function setPercentage(uint _percentage) external onlyOwner{
 		Percentage = _percentage / 100 ;
 	}
 
-	function listToken(address token, uint tokenId, uint price, uint256 quantity) external {
-        require(price > 0, "Price must be greater than 0");
+	function listToken(address token, uint tokenId, uint price, uint256 tokenAmount, uint256 quantity) external {
+        require(price >= 0, "Price must be equal or greater than 0");
+		require(tokenAmount >=0, "Token amount must be equal or greater than 0");
+		
+		if(price == 0){
+			require(tokenAmount > 0, "Token amount must be greator than 0 if it is sale by native token");
+		}else if(tokenAmount == 0){
+			require(price > 0, "Price must be gretor than 0 if it is a sale by bnb");
+		}
+
 		ERC1155(token).safeTransferFrom(msg.sender, address(this), tokenId, quantity,"");
 
 		Listing memory listing = Listing(
@@ -65,7 +145,8 @@ contract Market {
 			msg.sender,
 			token,
 			tokenId,
-			price
+			price,
+			tokenAmount
 		);
 
 		_listingId++;
@@ -78,6 +159,7 @@ contract Market {
 			token,
 			tokenId,
 			price,
+			tokenAmount,
             quantity
 		);
 	}
@@ -86,7 +168,7 @@ contract Market {
 		return _listings[listingId];
 	}
 
-	function buyToken(uint listingId, uint256 quantity) external payable {
+	function buyNFTWithBNB(uint listingId, uint256 quantity) external payable {
 		Listing storage listing = _listings[listingId];
 
 		require(msg.sender != listing.seller, "Seller cannot be buyer");
@@ -94,15 +176,14 @@ contract Market {
 
 		require(msg.value >= listing.price, "Insufficient payment");
 
-
 		ERC1155(listing.token).safeTransferFrom(address(this), msg.sender, listing.tokenId, quantity,"");
 		
-		if(listing.seller == Owner){
+		if(listing.seller == nftOwner){
 			payable(listing.seller).transfer(listing.price);
 		}
 		else{
-			payable(Owner).transfer(Percentage * listing.price); //Transfer the commision to the site owner
-			payable(listing.seller).transfer(listing.price);     //Transfer the payment to the seller
+			payable(nftOwner).transfer(Percentage * listing.price); //Transfer the commision to the site nft owner
+			payable(listing.seller).transfer(listing.price - Percentage * listing.price);     //Transfer the payment to the seller
 		}
 		emit Sale(
 			listingId,
@@ -110,6 +191,41 @@ contract Market {
 			listing.token,
 			listing.tokenId,
 			listing.price,
+			0,
+            quantity
+		);
+	}
+
+
+	function buyNFTWithToken(uint listingId, uint256 quantity) external {
+		Listing storage listing = _listings[listingId];
+
+		require(msg.sender != listing.seller, "Seller cannot be buyer");
+		require(listing.status == ListingStatus.Active, "Listing is not active");
+
+		require(ERC20(nativeToken).balanceOf(msg.sender) >= listing.tokenAmount, "Insufficient tokens");
+
+		ERC1155(listing.token).safeTransferFrom(address(this), msg.sender, listing.tokenId, quantity,"");
+		
+		if(listing.seller == nftOwner){
+			// transfer native token from users's wallet to seller wallet, here owner and seller are same
+			ERC20(nativeToken).transferFrom(msg.sender, address(this), listing.tokenAmount);
+			ERC20(nativeToken).transfer(listing.seller, listing.tokenAmount);
+		}
+		else{
+			// transfer native token from user's wallet to nftowner and seller
+			ERC20(nativeToken).transferFrom(msg.sender, address(this), listing.tokenAmount);
+			ERC20(nativeToken).transfer(nftOwner, Percentage * listing.tokenAmount);
+			ERC20(nativeToken).transfer(listing.seller, listing.tokenAmount - Percentage * listing.tokenAmount );
+		}
+
+		emit Sale(
+			listingId,
+			msg.sender,
+			listing.token,
+			listing.tokenId,
+			0,
+			listing.tokenAmount,
             quantity
 		);
 	}
@@ -125,6 +241,10 @@ contract Market {
 		ERC1155(listing.token).safeTransferFrom(address(this), msg.sender, listing.tokenId,quantity,"");
 
 		emit Cancel(listingId, listing.seller);
+	}
+
+	function getNativeToken() public view returns(address){
+		return nativeToken;
 	}
 
 	function onERC1155Received(address, address, uint256, uint256, bytes memory) public virtual returns (bytes4) {
